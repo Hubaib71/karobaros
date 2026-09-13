1d36c261b4f1cbae36d6b13783133b5021b611d4
Macbook Pro <macbookpro@Macbooks-MBP.bbrouter>
2026-09-13 21:48:44 +0500

    fix: allow Vercel frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://karobaros-jade.vercel.app",
 ],
    
     allow_credentials=True,
